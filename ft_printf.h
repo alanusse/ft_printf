161c53e587f4e7193ac12bab3609cab8c8a8498c
Agustin Lanusse <lanusseagustin@gmail.com>
2023-10-25 20:42:53 +0200

    modified putchar

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 22:21:53 by aglanuss          #+#    #+#             */
-/*   Updated: 2023/10/25 02:11:12 by aglanuss         ###   ########.fr       */
+/*   Updated: 2023/10/25 20:32:59 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ typedef struct s_print
 }					t_print;
 
 int		ft_printf(char const *format, ...);
-int		ft_putchar(char c);
+void	ft_putchar(t_print *tab, char c);
 int		ft_putstr(char *str);
 int		ft_putnbr(int nb);
 int		ft_putnbr_unsigned(unsigned int nb);
--- ft_putchar.c
+++ ft_putchar.c
@@ -6,15 +6,19 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 23:33:48 by aglanuss          #+#    #+#             */
-/*   Updated: 2023/10/25 02:00:08 by aglanuss         ###   ########.fr       */
+/*   Updated: 2023/10/25 20:32:28 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putchar(char c)
+void	ft_putchar(t_print *tab, char c)
 {
-	if (write(1, &c, 1) != 1)
-		return (-1);
-	return (1);
+	if (write(1, &c, 1) == -1)
+	{
+		tab->error = 1;
+		tab->length = -1;
+	}
+	else
+		tab->length += 1;
 }
