672286033921d55ef767c772f031b22cc5108400
Agustin Lanusse <lanusseagustin@gmail.com>
2023-10-25 20:53:14 +0200

    added comment to putchar

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,12 +6,16 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 23:33:48 by aglanuss          #+#    #+#             */
-/*   Updated: 2023/10/25 20:49:45 by aglanuss         ###   ########.fr       */
+/*   Updated: 2023/10/25 20:52:27 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+/**
+ * Print a char.
+ * If write fails, sets tab error to 1 and length to -1, and return 0.
+*/
 int	ft_putchar(t_print *tab, char c)
 {
 	if (write(1, &c, 1) == -1)
