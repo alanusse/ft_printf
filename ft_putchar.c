fae1c53d8b9eb7045cae9398ed771d6b2ba02d2e
Agustin Lanusse <lanusseagustin@gmail.com>
2023-10-26 11:13:18 +0200

    modified putchar

--- ft_putchar.c
+++ ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: aglanuss <aglanuss@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 23:33:48 by aglanuss          #+#    #+#             */
-/*   Updated: 2023/10/25 23:27:18 by aglanuss         ###   ########.fr       */
+/*   Updated: 2023/10/26 11:12:14 by aglanuss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,15 @@
 
 /**
  * Print a char.
- * If write fails, sets tab error to 1 and length to -1, and return 0.
+ * If write fails, sets tab error to 1 and length to -1.
  * 
 */
-int	ft_putchar(t_print *tab, char c)
+void	ft_putchar(t_print *tab, char c)
 {
 	if (write(1, &c, 1) == -1)
 	{
 		tab->error = 1;
 		tab->length = -1;
-		return (0);
 	}
 	tab->length += 1;
-	return (1);
 }
