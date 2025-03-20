244ce353ad3f6da3090a07d7af5529a42b9682a7
alanusse <lanusseagustin@gmail.com>
2025-03-20 16:02:04 +0100

    fix norminette

--- src/ft_putchar.c
+++ src/ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: agulanus <agulanus@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/20 14:20:19 by agulanus          #+#    #+#             */
-/*   Updated: 2025/03/20 14:26:21 by agulanus         ###   ########.fr       */
+/*   Updated: 2025/03/20 16:01:33 by agulanus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,8 @@
  */
 void	ft_putchar(t_ft_printf *pf_state, char c)
 {
-  if (write(1, &c, 1) == 1)
-    pf_state->length++;
-  else
-    pf_state->is_error = true;
+	if (write(1, &c, 1) == 1)
+		pf_state->length++;
+	else
+		pf_state->is_error = true;
 }
